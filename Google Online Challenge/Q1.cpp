d3456f22104970d81bded79ceeffb3ecc9b4160d
Lee Cheng Hui <57316431+chenghui-lee@users.noreply.github.com>
2020-12-23 22:40:56 +0800

    Create Q1.cpp

---
+++ Google Online Challenge/Q1.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#define load ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define srt(v) sort(v.begin(), v.end())
+#define MOD 1000000007
+const int INF = 1e9;
+const double PI = 3.14159265358979323846;
+ll gcd(ll a, ll b) {
+    return b != 0 ? gcd(b, a % b) : a;
+}
+ll lcm(ll a, ll b) {
+    return (a * b) / gcd(a, b);
+}
+ll spow(ll a, ll b, ll m){
+	ll res = 1;
+	while(b){
+		if (b&1) res = res*a%m;
+		a = a*a%m;
+		b /= 2;
+	}
+	return res;
+}
+ll divide(ll a, ll b){
+	return a * spow(b, MOD-2, MOD) % MOD;
+}
+template<typename T> void print(const T &x){cout<<x<<"\n";}
+template<typename T=int> vector<T> read(size_t n){
+  vector<T> arr(n);
+  for(size_t i=0;i<n;i++) cin>>arr[i];
+  return arr;
+}
+
+
+void solve(){
+	int a, delays;
+	cin >> a >> delays;
+	unordered_map<char, set<char>> onme;
+	set<char> res;
+	
+	for (int i = 0; i < a; i++){
+		char dep, indep;
+		cin >> dep >> indep;
+		onme[indep].insert(dep);
+	}
+	queue<char> process;
+	for (int i = 0; i < delays; i++){
+		char del;
+		cin >> del;
+		process.push(del);
+	}
+
+	while(process.size()){
+		int t = process.size();
+		for (int i = 0; i < t; i++){
+			char temp = process.front();
+			process.pop();
+			res.insert(temp);
+			for(auto x : onme[temp]){
+				process.push(x);
+			}
+		}
+	}
+	for(auto& x : res){
+		cout << x << " ";
+	}
+	cout << "\n";
+	
+	
+	
+}
+
+int main(){
+	load;
+	//int t = 1;
+	int t;
+	cin >> t;
+	for(int i=1; i<=t; i++){
+		cout << "Case #" << i << ": ";
+		solve();
+	}
+	return 0;
+
+}
+
